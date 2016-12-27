542c309400e0e60e3642205da5b8267f63721bfb
Sylwia <sylviaskovron@gmail.com>
2016-12-27 19:49:29 +0100

    Add check_for_palindromes.js

---
+++ check_for_palindromes.js
@@ -0,0 +1,16 @@
+function palindrome(str) {
+  
+  /* remove special characters, spaces and make lowercase*/
+  var removeChar = str.replace(/[^A-Z0-9]/ig, "").toLowerCase();
+
+  /* reverse removeChar for comparison*/
+  var checkPalindrome = removeChar.split('').reverse().join('');
+
+  /* Check to see if str is a Palindrome*/
+   return (removeChar === checkPalindrome);
+  
+}
+
+
+
+palindrome("eye");
